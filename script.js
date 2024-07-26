e515188f765390eda13f5127e4eba5c7e8169e44
imthe27 <fertrevi02@gmail.com>
2024-07-26 15:52:34 -0600

    added css stylesheet and event
    i've added the stylesheet with the flexbox attribute to the container
    and linked it in the html file, also added the function in the divs
    event that changes the background color.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const container = document.querySelector("#container");
 
+
+
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
-    div.setAttribute("id", i.valueOf());
+    div.setAttribute("class", "grid");
+    div.addEventListener("mouseenter", (e) => {
+        e.target.style.background = "blue";
+    })
     container.appendChild(div)
 }
