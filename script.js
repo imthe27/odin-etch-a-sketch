95746a05bf25957db306e325eb5197115d8b1a37
imthe27 <fertrevi02@gmail.com>
2024-07-26 20:09:17 -0600

    added css stylesheet and event
    i've added the stylesheet with the flexbox attribute to the container
    and linked it in the html file, also added the function in the divs
    event that changes the background color.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+const body = document.querySelector("body");
 const container = document.querySelector("#container");
+const divbtn = document.createElement("div");
+const btn = document.createElement("button");
+let input = '';
 
-
-
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.setAttribute("class", "grid");
-    div.addEventListener("mouseenter", (e) => {
-        e.target.style.background = "blue";
-    })
-    container.appendChild(div)
+function newGrid(square) {
+    for (let i = (square*square);i > 0; i--) {
+        const div = document.createElement("div");
+        let basis = `${100/square}%`;
+        console.log(basis);
+        div.style.flex = 1, 0, "auto";
+        div.addEventListener("mouseenter", (e) => {
+            e.target.style.background = "blue";
+        })
+        container.appendChild(div)
+    }
 }
+
+btn.textContent = "New Grid";
+btn.addEventListener("click", () => {
+    input = prompt("Type the desired size:", "16");
+    newGrid(input);
+})
+divbtn.appendChild(btn);
+body.insertBefore(divbtn, body.firstChild);
