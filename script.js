ac8a2b3b9f6d2860e5530dae17b36cb1e3fe8131
imthe27 <fertrevi02@gmail.com>
2025-11-27 17:44:39 -0600

    added final tweaks, wrapper to center top button and input filtering logic

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const body = document.querySelector("body");
 const container = document.querySelector("#container");
 const divbtn = document.createElement("div");
+const btnwrapper = document.createElement("div");
 const btn = document.createElement("button");
+btn.id = "button";
 const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
 let input = '';
 
@@ -30,10 +32,21 @@ function newGrid(square) {
 
 btn.textContent = "New Grid";
 btn.addEventListener("click", () => {
-    input = prompt("Type the desired size:", "16");
+    do {
+        input = prompt("Type the desired size(keep it below 100):", "16");
+    } while (input > 100 || input < 1 || isNaN(input) || input === null || input.trim() === '');
     newGrid(input);
 })
+btn.style.flex = "1 1 fit-content";
 
 divbtn.appendChild(btn);
-body.insertBefore(divbtn, body.firstChild);
+btnwrapper.appendChild(divbtn);
+btnwrapper.style.display = "flex";
+btnwrapper.style.justifyContent = "center";
+btnwrapper.style.width = "100%";
+divbtn.style.display = "flex";
+divbtn.style.width = "fit-content";
+divbtn.style.justifyContent = "center";
+divbtn.style.marginBottom = "7px";
+body.insertBefore(btnwrapper, body.firstChild);
 newGrid(16);
